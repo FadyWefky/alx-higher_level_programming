546ecb36e3a866122867f7a4d60a06138a71c549
FadyWefky <fady.wefky33@gmail.com>
2023-06-30 01:47:57 +0300

    Done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this module create the class square with size"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size):
+        """__size: Private attribute"""
+        self.__size = size
